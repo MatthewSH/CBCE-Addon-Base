b09e82a60a7a8f51f3d41b3d815cc02b205b305c
MatthewSH <MatthewSH@users.noreply.github.com>
2018-08-22 19:56:15 -0400

    Adding chatbotce package imports.

--- src/Addon.ts
+++ src/Addon.ts
@@ -1,11 +1,15 @@
+import { API } from "chatbotce/typings/lib/API";
+import { Helper } from "chatbotce/typings/lib/Helpers";
+import { PubSub } from "chatbotce/typings/lib/PubSub";
+
 export var commands: Array<string> = [];
 
-let api;
-let helper;
+let api: API;
+let helper: Helper;
 let log;
-let pubsub;
+let pubsub: PubSub;
 
-export function constructor(api: any, helper: any, log: any, pubsub: any) {
+export function constructor(api: API, helper: Helper, log: any, pubsub: PubSub) {
     this.api = api;
     this.helper = helper;
     this.log = log;
